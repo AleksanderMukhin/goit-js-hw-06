63b0b440502e21df31ce2f008f2ad254bde0be8a
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-02-23 14:23:44 +0100

    task-1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,6 +1,12 @@
 
-const liItem = document.querySelectorAll('.item');
+const categoriesList = document.querySelectorAll('#categories');
+const categoriesItems = document.querySelectorAll('li.item');
 
-console.log(`Quantity li.item: ${liItem.length}`);
+console.log(`Number of categories: ${categoriesItems.length}`);
 
-
+categoriesItems.forEach(item => {
+  const categoryName = item.querySelector('h2').textContent;
+  console.log(`Category: ${categoryName}`);
+  const quantityCotegoriesItem = item.querySelectorAll('li').length;
+  console.log(`Elements: ${quantityCotegoriesItem}`)
+});
