06cd3b160b90060170717c4b1f0fa42f7d268ee8
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-02-28 21:55:54 +0100

    task8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const formEl = event.currentTarget.elements;
+  
+  const email = formEl.email.value;
+  const password = formEl.password.value;
+  const formData = {    
+    email,
+    password,
+  }
+  console.log(formData);
+
+  if (email === "" || password === "") {
+    alert ("Please fill in all the fields!");
+  };
+
+  event.currentTarget.reset();
+  }
