ded28665bb1c7a3d4efd8ec19974387889114138
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-03-01 11:12:04 +0100

    task9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
+const btnChangeColor = document.querySelector('.change-color');
+const textColor = document.querySelector('.color')
+
+btnChangeColor.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  textColor.textContent = color;
+})
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
