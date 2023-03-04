f74835ac91fac37abab62c2852d0d040123c4106
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-03-04 11:58:47 +0100

    fixed

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,32 @@
 
-const categoriesList = document.querySelectorAll('#categories');
-const categoriesItems = document.querySelectorAll('li.item');
+const categoriesList = document.querySelector('#categories');
+
+// const categoriesItems = document.querySelectorAll('li.item');
+const categoriesItems = [...categoriesList.children];
+console.log("🚀 categoriesItems:", categoriesItems)
 
 console.log(`Number of categories: ${categoriesItems.length}`);
 
 categoriesItems.forEach(item => {
-  const categoryName = item.querySelector('h2').textContent;
+  const categoryName = item.firstElementChild.textContent;
   console.log(`Category: ${categoryName}`);
-  const quantityCotegoriesItem = item.querySelectorAll('li').length;
+  const quantityCotegoriesItem = item.children.length;
   console.log(`Elements: ${quantityCotegoriesItem}`)
 });
+
+
+
+// const categoriesList = document.querySelector('#categories');
+
+// const categoriesItems = document.querySelectorAll('li.item');
+
+// console.log("🚀 categoriesItems:", categoriesItems)
+
+// console.log(`Number of categories: ${categoriesItems.length}`);
+
+// categoriesItems.forEach(item => {
+//   const categoryName = item.querySelector('h2').textContent;
+//   console.log(`Category: ${categoryName}`);
+//   const quantityCotegoriesItem = item.querySelectorAll('li').length;
+//   console.log(`Elements: ${quantityCotegoriesItem}`)
+// });
