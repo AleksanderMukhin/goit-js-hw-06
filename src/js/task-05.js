2dcd98ac7d47c3c8d3ab93ec42a4e6d04039ebdc
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-03-04 08:48:43 +0100

    fix

--- src/js/task-05.js
+++
@@ -1,5 +0,0 @@
-const inputRef = document.querySelector('#name-input');
-const outputRef = document.querySelector('#name-output');
-
-inputRef.addEventListener('input', () =>
-  outputRef.textContent = inputRef.value || 'Anonymous');
--- src/js/task-07.js
+++
@@ -1,9 +0,0 @@
-const inputSize = document.querySelector('#font-size-control');
-const text = document.querySelector('#text');
-
-inputSize.addEventListener('input', () => {
-  const size = inputSize.value;
-console.log(size)
-  text.style.fontSize = `${size}px`;
-}  
-)
